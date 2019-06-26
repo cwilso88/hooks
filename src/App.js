6bb2557f4047206ec966f159ae72d05bc1c3875d
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-26 11:27:35 -0400

    Updated setResource in app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import ResourceList from './components/ResourceList';
 
 const App = () => {
-  const [resource, setResource] = useState('These are my posts');
+  const [resource, setResource] = useState('posts');
     return (
       <div className="App">
         <div>
-          <button onClick={() => setResource('These are my posts')}>Posts</button>
-          <button onClick={() => setResource('Look at my todos')}>ToDos</button>
+          <button onClick={() => setResource('posts')}>Posts</button>
+          <button onClick={() => setResource('todos')}>ToDos</button>
         </div>
        <ResourceList resource={resource}/>
       </div>
