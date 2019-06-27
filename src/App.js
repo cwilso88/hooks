08a6f10379378d2f1ce71d8624cb745c91f328e6
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-26 22:00:10 -0400

    Added in the componentDidUpdate lifecycle method to update set

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import ResourceList from './components/ResourceList';
 
 const App = () => {
-  const [resource, setResource] = useState('posts');
+  const [resource, setResource] = useState('todos');
     return (
-      <div className="App">
+      <div>
         <div>
           <button onClick={() => setResource('posts')}>Posts</button>
           <button onClick={() => setResource('todos')}>ToDos</button>
--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -9,10 +9,19 @@ class ResourceList extends Component {
 
         this.setState({ resources: response.data });
     }
+
+    async componentDidUpdate(prevProps) {
+        if(prevProps.resource !== this.props.resource) {
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+
+            this.setState({ resources: response.data });
+        }
+    }
+
     render() {
         return (
             <div>
-                {this.state.resources.length}
+               Hi {this.state.resources.length}
             </div>
         )
     }
